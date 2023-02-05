e9b0063fd049d2f094dfeb2e908d996192950c45
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-02-06 01:26:33 +0500

    JavaScript-Chap-6to9
    JavaScript assignment 4 Chap 09-11 second question complete.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Chap #09-11
 
 // Question #01
-var city = prompt("Enter your city");
-if(city === "Karachi"){
-    document.write("<h1>Welcome to city lights</h1>")
-}
+// var city = prompt("Enter your city");
+
+// if(city === "Karachi"){
+//     document.write("<h1>Welcome to city lights</h1>")
+// }
+
+// Question #02
+// var gender = prompt("Enter Your Gender");
+
+// if(gender === "male"){
+//     document.write("Good Morning Sir");
+// }
+// if(gender === "female"){
+//     document.write("Good Morning Ma'am");
+// }
 
 // Question #02
